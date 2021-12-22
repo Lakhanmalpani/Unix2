e969d302f8631aafd3d316c6c23dc4f43983bdc7
Lakhanmalpani <96461291+Lakhanmalpani@users.noreply.github.com>
2021-12-22 11:30:58 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,30 @@
+/*
+  Objective : its a bank transfer code
+  Author : Vishhal
+  Version : 1.0
+  JDK     : java 11
+  Database: Oracle
+  Team    : 3
+  Date  : 22-Dec 2021
+  Time    : 11:00
+*/
+interface Transferring
+{
+
+        public void transfer(SavinsgAccount sa, CurrentAccount ca, float amt);
+}
+
+class FundTransfer implements Transferring
+{
+        public void transfer(SavinsgAccount sa, CurrentAccount ca, float amt)
+        {
+
+        }
+}
+public class First
+{
+        public static void main(String args[])
+        {
+
+        }
+}
